b64ec8c55ef3e7190bcfc325fb1b955cd0dace49
Emmanuelronoh <eronoh036@gmail.com>
2024-10-25 08:09:08 +0300

    first commit

--- server/models.py
+++ server/models.py
@@ -1,60 +1,55 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from sqlalchemy.orm import validates
-from sqlalchemy.ext.associationproxy import association_proxy
-from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
-convention = {
-    "ix": "ix_%(column_0_label)s",
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_%(constraint_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
-}
+db = SQLAlchemy()
 
-metadata = MetaData(naming_convention=convention)
-
-db = SQLAlchemy(metadata=metadata)
-
-
-class Planet(db.Model, SerializerMixin):
-    __tablename__ = 'planets'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    distance_from_earth = db.Column(db.Integer)
-    nearest_star = db.Column(db.String)
-
-    # Add relationship
-
-    # Add serialization rules
-
-
-class Scientist(db.Model, SerializerMixin):
+class Scientist(db.Model):
     __tablename__ = 'scientists'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    field_of_study = Column(String, nullable=False)
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    field_of_study = db.Column(db.String)
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'field_of_study': self.field_of_study
+            # Missions omitted
+        }
 
-    # Add relationship
+class Planet(db.Model):
+    __tablename__ = 'planets'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    distance_from_earth = Column(Integer, nullable=True)
+    nearest_star = Column(String, nullable=True)
 
-    # Add serialization rules
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'distance_from_earth': self.distance_from_earth,
+            'nearest_star': self.nearest_star
+        }
 
-    # Add validation
-
-
-class Mission(db.Model, SerializerMixin):
+class Mission(db.Model):
     __tablename__ = 'missions'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    scientist_id = Column(Integer, ForeignKey('scientists.id'), nullable=False)
+    planet_id = Column(Integer, ForeignKey('planets.id'), nullable=False)
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    scientist = relationship('Scientist', backref='missions')
+    planet = relationship('Planet', backref='missions')
 
-    # Add relationships
-
-    # Add serialization rules
-
-    # Add validation
-
-
-# add any models you may need.
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'scientist_id': self.scientist_id,
+            'planet_id': self.planet_id
+        }
